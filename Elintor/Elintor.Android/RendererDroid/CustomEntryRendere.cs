fd6c01eeb3e3a0c9361734638e644e1968cbcfe4
apurvaraut <apurvaraut20@gmail.com>
2020-03-18 13:45:26 +0530

    Main repository project

---
+++ Elintor/Elintor.Android/RendererDroid/CustomEntryRendere.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Elintor.CustomRenderer;
+using Elintor.Droid.RendererDroid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRendere))]
+namespace Elintor.Droid.RendererDroid
+{
+    public  class CustomEntryRendere : EntryRenderer
+    {
+        public CustomEntryRendere(Context context) : base(context)
+        {
+            AutoPackage = false;
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.Background = new ColorDrawable(Android.Graphics.Color.White);
+
+            }
+        }
+    }
+}
